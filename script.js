ca6bd5c550f70617a5295cbd93c884578bab7b80
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-04 12:00:01 +0500

    variable syntax

---
+++ script.js
@@ -0,0 +1,14 @@
+let js ='amazing';
+if (js === 'amazing') alert ('javascript is fun!')
+console.log(20+210+41210-12430)
+
+
+let firstName="amin";
+console.log(firstName)
+
+let jonas_matilda='JM';
+let $function =24;
+let person ='jonas';
+let PI =3.1415;
+let myFirstJob='programmer';
+let mySecondJob='Teacher';
