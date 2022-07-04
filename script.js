f8a11176a96e2b4fcfe830daa62b547028ea7f56
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-04 16:20:56 +0500

    Basic Operators

--- script.js
+++ script.js
@@ -1,48 +1,73 @@
-let js ='amazing';
-if (js === 'amazing') alert ('javascript is fun!')
-console.log(20+210+41210-12430)
+// let js ='amazing';
+// if (js === 'amazing') alert ('javascript is fun!')
+// console.log(20+210+41210-12430)
 
+// let firstName="amin";
+// console.log(firstName)
 
-let firstName="amin";
-console.log(firstName)
+// let jonas_matilda='JM';
+// let $function =24;
+// let person ='jonas';
+// let PI =3.1415;
+// let myFirstJob='programmer';
+// let mySecondJob='Teacher';
 
-let jonas_matilda='JM';
-let $function =24;
-let person ='jonas';
-let PI =3.1415;
-let myFirstJob='programmer';
-let mySecondJob='Teacher';
+// let country ='Gapshapistan';
+// let contenent='Sub-continent';
+// let population='9000M'
 
+// console.log(country)
+// console.log(contenent)
+// console.log(population)
 
-let country ='Gapshapistan';
-let contenent='Sub-continent';
-let population='9000M'
+// let sevenPrimitiveDataTypes=()=>{'numbers','string','boolean','undefined','null','symbol','BigInt'};
+// let number=24;
+// let string='amin';
+// let boolean=true;
+// // let undefined;
+// // let => null;
 
-console.log(country)
-console.log(contenent)
-console.log(population)
+// /// Javvascript comment
+// let BigInt=('larger integer than the number ccan hold')
 
-let sevenPrimitiveDataTypes=()=>{'numbers','string','boolean','undefined','null','symbol','BigInt'};
-let number=24;
-let string='amin';
-let boolean=true;
-// let undefined;
-// let => null;
+// let petrol =true;
+// console.log(petrol);
 
-/// Javvascript comment
-let BigInt=('larger integer than the number ccan hold')
+// console.log(typeof true);
 
-let petrol =true;
-console.log(petrol);
+// petrol='ak sawa assi'
+// console.log(typeof petrol);
 
-console.log(typeof true);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-petrol='ak sawa assi'
-console.log(typeof petrol);
+// year=1993;
+// console.log(typeof year)
+//------------Math operators --------------
+const now = 2037;
+const ageAmin = now - 1993;
+const ageHuma = now - 1995;
+console.log(ageAmin, ageHuma);
 
-let year;
-console.log(year);
-console.log(typeof year);
+console.log(ageAmin * 2, ageAmin / 5, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3=2*2*2
 
-year=1993;
-console.log(typeof year)
+const firstName ='Amin';
+const secondName='ullah';
+console.log(firstName+ ' ' +secondName);
+//--------------Assignment Operators ---------
+let x=2+5; //7
+x +=10 // x= x+10
+x*=2 //x=x*2
+x++; //x+1
+x--;// x-1
+console.log(x)
+
+//----------Comparison operators
+
+console.log(ageAmin> ageHuma);//>,<,>=,<=
+console.log(ageHuma <= 18)
+
+const isFullAge=ageHuma >= 18;
+console.log(now - 1995 > now -2018)
