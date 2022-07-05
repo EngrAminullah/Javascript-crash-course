494f5cdecb0aa5a6ee6a0c91a920870cf6977b3b
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-05 15:20:59 +0500

    challenge

--- script.js
+++ script.js
@@ -1,85 +1,102 @@
-let js = "amazing";
-if (js === "amazing") alert("javascript is fun!");
-console.log(20 + 210 + 41210 - 12430);
+// let js = "amazing";
+// if (js === "amazing") alert("javascript is fun!");
+// console.log(20 + 210 + 41210 - 12430);
 
-let firstName = "amin";
-console.log(firstName);
+// let firstName = "amin";
+// console.log(firstName);
 
-let jonas_matilda = "JM";
-let $function = 24;
-let person = "jonas";
-let PI = 3.1415;
-let myFirstJob = "programmer";
-let mySecondJob = "Teacher";
+// let jonas_matilda = "JM";
+// let $function = 24;
+// let person = "jonas";
+// let PI = 3.1415;
+// let myFirstJob = "programmer";
+// let mySecondJob = "Teacher";
 
-let country = "Gapshapistan";
-let contenent = "Sub-continent";
-let population = "9000M";
+// let country = "Gapshapistan";
+// let contenent = "Sub-continent";
+// let population = "9000M";
 
-console.log(country);
-console.log(contenent);
-console.log(population);
+// console.log(country);
+// console.log(contenent);
+// console.log(population);
 
-let sevenPrimitiveDataTypes = () => {
-  "numbers", "string", "boolean", "undefined", "null", "symbol", "BigInt";
-};
-let number = 24;
-let string = "amin";
-let boolean = true;
-// let undefined;
-// let => null;
+// let sevenPrimitiveDataTypes = () => {
+//   "numbers", "string", "boolean", "undefined", "null", "symbol", "BigInt";
+// };
+// let number = 24;
+// let string = "amin";
+// let boolean = true;
+// // let undefined;
+// // let => null;
 
-/// Javvascript comment
-let BigInt = "larger integer than the number ccan hold";
+// /// Javvascript comment
+// let BigInt = "larger integer than the number ccan hold";
 
-let petrol = true;
-console.log(petrol);
+// let petrol = true;
+// console.log(petrol);
 
-console.log(typeof true);
+// console.log(typeof true);
 
-petrol = "ak sawa assi";
-console.log(typeof petrol);
+// petrol = "ak sawa assi";
+// console.log(typeof petrol);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1993;
-console.log(typeof year);
-//------------Math operators --------------
-const now = 2037;
-const ageAmin = now - 1993;
-const ageHuma = now - 1995;
-console.log(ageAmin, ageHuma);
+// year = 1993;
+// console.log(typeof year);
+// //------------Math operators --------------
+// const now = 2037;
+// const ageAmin = now - 1993;
+// const ageHuma = now - 1995;
+// console.log(ageAmin, ageHuma);
 
-console.log(ageAmin * 2, ageAmin / 5, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3=2*2*2
+// console.log(ageAmin * 2, ageAmin / 5, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3=2*2*2
 
-const ffirstName = "Amin";
-const secondName = "ullah";
-console.log(ffirstName + " " + secondName);
-//--------------Assignment Operators ---------
-// let x = 2 + 5; //7
-// x += 10; // x= x+10
-// x *= 2; //x=x*2
-// x++; //x+1
-// x--; // x-1
-// console.log(x);
+// const ffirstName = "Amin";
+// const secondName = "ullah";
+// console.log(ffirstName + " " + secondName);
+// //--------------Assignment Operators ---------
+// // let x = 2 + 5; //7
+// // x += 10; // x= x+10
+// // x *= 2; //x=x*2
+// // x++; //x+1
+// // x--; // x-1
+// // console.log(x);
 
-//----------Comparison operators
+// //----------Comparison operators
 
-console.log(ageAmin > ageHuma); //>,<,>=,<=
-console.log(ageHuma <= 18);
+// console.log(ageAmin > ageHuma); //>,<,>=,<=
+// console.log(ageHuma <= 18);
 
-const isFullAge = ageHuma >= 18;
-console.log(now - 1995 > now - 2018);
-console.log(25 - 10 - 5);
-//---------operator precedence----------------
-let x, y;
-x=y=25-10-5;
-console.log(x, y)
+// const isFullAge = ageHuma >= 18;
+// console.log(now - 1995 > now - 2018);
+// console.log(25 - 10 - 5);
+// //---------operator precedence----------------
+// let x, y;
+// x=y=25-10-5;
+// console.log(x, y)
 
-const averageAge= ageAmin+ageHuma/2;
-console.log(ageAmin, ageHuma, averageAge)
-const FaverageAge= (ageAmin+ageHuma)/2;
-console.log(ageAmin, ageHuma, FaverageAge)
+// const averageAge= ageAmin+ageHuma/2;
+// console.log(ageAmin, ageHuma, averageAge)
+// const FaverageAge= (ageAmin+ageHuma)/2;
+// console.log(ageAmin, ageHuma, FaverageAge)
+
+//-----------------Coding challenge##1------------
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHiegherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHiegherBMI);
