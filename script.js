c6a9e1de8a8706024b7297804fd8674ad13656db
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-06 18:20:13 +0500

    Truthy and falsy values

--- script.js
+++ script.js
@@ -171,4 +171,18 @@ console.log(Boolean(0));
 console.log(Boolean(undefined))
 console.log(Boolean('Amin'))
 console.log(Boolean({}))
-console.log('')
+console.log(Boolean(''))
+
+const money=0;
+if(money){
+    console.log("Don't spend it all!")
+}else{
+    console.log("go and get a job!")
+}
+
+let height;
+if(height){
+    console.log('Yay! Height is defined')
+}else{
+    console.log('Height is undefined')
+}
