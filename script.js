7dea3641ffafa4373a239253d0c53b41162234df
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-06 17:48:54 +0500

    Type Coercion

--- script.js
+++ script.js
@@ -145,10 +145,22 @@ if(BMIMark>BMIJohn){
 // }
 // console.log(century)
 
-//------------------Conversion---------------
+//------------------ Type Conversion---------------
 const inputYear='1991';
 console.log(Number(inputYear), inputYear)
 console.log(Number(inputYear)+18)
 console.log(Number('Amin'))
 console.log(typeof NaN)
 console.log(String(23), 23)
+
+
+//----------------- type Coercion------------------
+
+console.log('I am ' + 23 +' years old')
+console.log('23'-'10'-3)
+console.log('12' * 2)
+console.log('23'/'2')
+
+let n='1' +1;
+n=n-1;
+console.log(n)
