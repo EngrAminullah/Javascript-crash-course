0629da94fea94eec56ccfa986342b7e40ccba757
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-06 15:18:04 +0500

    if else statement

--- script.js
+++ script.js
@@ -122,10 +122,20 @@
 //  lines💕`)
 
 
-const age=19;
-if(age>=18){
-    console.log(`Sarah can start driving license`)
+// const age=19;
+// if(age>=18){
+//     console.log(`Sarah can start driving license`)
+// }else{
+//     const yearsLeft=18-age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)!`)
+// }
+
+const birthYear=2001;
+
+let century;
+if(birthYear <= 2000){
+    century=20;
 }else{
-    const yearsLeft=18-age;
-    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)!`)
+    century=21;
 }
+console.log(century)
