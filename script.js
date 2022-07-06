0f9ab4c2f02f56ac3b36d6f136294326a1e993e7
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-06 18:09:48 +0500

    5 falsy values

--- script.js
+++ script.js
@@ -164,3 +164,11 @@ console.log('23'/'2')
 let n='1' +1;
 n=n-1;
 console.log(n)
+
+//--------------5 falsy values--------------------
+//---0,'',undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined))
+console.log(Boolean('Amin'))
+console.log(Boolean({}))
+console.log('')
