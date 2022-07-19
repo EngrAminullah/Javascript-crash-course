81bbfaaeb1fee629798e64b72c2b8fb3f95665c5
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-19 13:20:54 +0500

    Functions calling other functions

--- script.js
+++ script.js
@@ -326,26 +326,38 @@
 // console.log(appleOrangeJuice)
 
 //-------------Function declaration---------------
-function calcAge1(birthYear){
-    return 2037-birthYear
-}
-const age1=calcAge1(1991);
-//---------function expression-------------
-const calcAge2=function(birthYear){
-    return 2037-birthYear;
-}
-const age2=calcAge2(1991)
-console.log(age1, age2)
-//----------Arrow function----------
-const calcAge3= (birthYear) => 2037 -birthYear;
-const age3= calcAge3(1991);
-console.log(age3)
+// function calcAge1(birthYear){
+//     return 2037-birthYear
+// }
+// const age1=calcAge1(1991);
+// //---------function expression-------------
+// const calcAge2=function(birthYear){
+//     return 2037-birthYear;
+// }
+// const age2=calcAge2(1991)
+// console.log(age1, age2)
+// //----------Arrow function----------
+// const calcAge3= (birthYear) => 2037 -birthYear;
+// const age3= calcAge3(1991);
+// console.log(age3)
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age=2037 -birthYear;
-    const retirement =65-age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years`
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age=2037 -birthYear;
+//     const retirement =65-age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`
+// }
+// console.log(yearsUntilRetirement(1991, 'Amin'))
+// console.log(yearsUntilRetirement(1995, 'Bacha'))
+//--------------Functions calling other functions-----------------
+function cutFruirPieces(fruit){
+    return fruit * 4
 }
-console.log(yearsUntilRetirement(1991, 'Amin'))
-console.log(yearsUntilRetirement(1995, 'Bacha'))
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruirPieces(apples);
+    const orangePieces = cutFruirPieces(oranges)
+    console.log(apples, oranges);
+    const juice = `juice with ${applePieces} piece of apple and ${orangePieces} pieces of oranges.`;
+    return juice;
+  }
+  console.log(fruitProcessor(2,3))
