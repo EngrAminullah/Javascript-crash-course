5dd4db598ba316ceb0ea9028ae6c406a372f19bc
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-20 13:12:04 +0500

    objects method challenge

--- script.js
+++ script.js
@@ -557,10 +557,17 @@ const amin={
         calcAge: function(){
             this.age= 2037 - this.birthYear;
             return this.age;
+        },
+        getSummary:function(){
+            return `${this.firstName} is a ${this.calcAge()} old ${this.job}, and he has ${this.hasDriversLicense ? 'a':'no'} driver's license`
         }
     };
-    console.log(amin.age);
-    console.log(amin.age);
-    console.log(amin.age);
     console.log(amin.calcAge());
+    console.log(amin.age);
+    console.log(amin.age);
+    console.log(amin.age);
     // console.log(amin['calcAge'](1991))
+
+    //Challenge
+    //"amin is a 46 years old teacher. and he has a driver license"
+    console.log(amin.getSummary())
