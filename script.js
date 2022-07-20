15e3093975111cc0d8abfe3fa1e528e6f08c7541
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-20 10:38:24 +0500

    Objects

--- script.js
+++ script.js
@@ -480,10 +480,26 @@
 
 //------------------arrays challenge--------------------
 
-const calcTip=function(bill){
-    return bill >=50 && bill <=300 ?bill *0.15:bill*0.2;
+// const calcTip=function(bill){
+//     return bill >=50 && bill <=300 ?bill *0.15:bill*0.2;
+// }
+// const bills=[125,555,44];
+// const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+// const totals=[bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]]
+// console.log(bills,tips,totals )
+
+const aminArray =[
+    'Amin',
+    'Ullah',
+    2037-1991,
+    'teacher',
+    ['Bacha','Sajid','Mansoor']
+];
+const amin={
+    firstName:'Amin',
+    lastName:'Ullah',
+    age:2037-1991,
+    job:'teacher',
+    friends:['Bacha','Sajid','Mansoor']
 }
-const bills=[125,555,44];
-const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
-const totals=[bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]]
-console.log(bills,tips,totals )
+
