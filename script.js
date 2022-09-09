343d63120a879ddd63df6b73adb2d21412270f2a
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-09-09 12:55:23 +0500

    Destructuring Arrays

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 // let js = "amazing";
 // if (js === "amazing") alert("javascript is fun!");
 // console.log(20 + 210 + 41210 - 12430);
@@ -729,28 +730,83 @@
 // console.log(calcAge(1991));
 
 /////------------- The this keyword---------------
-console.log(this);
+// console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAge(1993);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1993);
 
-const calcAgeArrow = (birthYear) => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(1993);
+// const calcAgeArrow = (birthYear) => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(1993);
 
-const person = {
-  firstName: "Amin",
-  secondName: "Ullah",
-  id: 5522,
-  fullName: function () {
-    return this.firstName + " " + this.secondName;
+// const person = {
+//   firstName: "Amin",
+//   secondName: "Ullah",
+//   id: 5522,
+//   fullName: function () {
+//     return this.firstName + " " + this.secondName;
+//   },
+// };
+// let pes = person;
+
+// console.log("person", person.fullName());
+
+////-----------Destructuring Arrays--------------
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tevanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetrian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
-let pes = person;
 
-console.log("person", person.fullName());
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second, third] = restaurant.categories;
+console.log(first, third, second);
+const [awal, , dwayam] = restaurant.starterMenu;
+console.log(awal, dwayam);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+////-------------Switching Variables----------
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+console.log(restaurant.order(2, 0));
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+////-----------Nested destructuring------------
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+/////-----------Default----------------------
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
