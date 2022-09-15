b3bb65a95aeeb2ae3d82a82113012fbe60cd62d2
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-09-15 12:34:38 +0500

    The Nullish Coalescing Operator(??)

--- script.js
+++ script.js
@@ -860,34 +860,44 @@ const restaurant = {
     console.log(otherIngredient);
   },
 };
-console.log("-------------- OR -----------");
-////--------Use ANY data type, return ANY data type, short-circuiting----------
+////----------------------The Nullish Coalescing Operator(??)-----------------
 
-console.log(3 || "Jonas");
-console.log("" || "Jonas");
-console.log(true || 0);
-console.log(undefined || null);
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
 
-console.log(undefined || 0 || "" || "Hello" || 23 || null);
+///Nullish: null and undefined(NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// console.log("-------------- OR -----------");
+// ////--------Use ANY data type, return ANY data type, short-circuiting----------
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// console.log(3 || "Jonas");
+// console.log("" || "Jonas");
+// console.log(true || 0);
+// console.log(undefined || null);
 
-console.log("-------------- AND -----------");
-console.log(0 && "Jonas");
-console.log(7 && "Jonas");
+// console.log(undefined || 0 || "" || "Hello" || 23 || null);
 
-console.log("Hello" && 23 && null && "jonas");
-//practical example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza("mushrooms", "spinach");
-}
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log("-------------- AND -----------");
+// console.log(0 && "Jonas");
+// console.log(7 && "Jonas");
+
+// console.log("Hello" && 23 && null && "jonas");
+// //practical example
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza("mushrooms", "spinach");
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
 
 // /////-----------------------Rest Pattern & Parameters---------------
 // //// 1)-------Destructuring-------------------------------
