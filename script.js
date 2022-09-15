1c17609020217e0d631da055c96bbecdb8753df0
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-09-15 13:47:54 +0500

    Logical Assignment Operators

--- script.js
+++ script.js
@@ -860,15 +860,47 @@ const restaurant = {
     console.log(otherIngredient);
   },
 };
-////----------------------The Nullish Coalescing Operator(??)-----------------
+////////------------Logical Assignment Operator-------------------
+const rest1 = {
+  name: "Capri",
+  // numGuests: 20,
+  numGuests: 0,
+};
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+////OR assignment Operator
+//rest1.numGuests = rest1.numGuests || 10;
+//rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
+////Nullish assignment Operator(Null or undefined)
 
-///Nullish: null and undefined(NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+////------------AND assignment Operator
+
+// rest1.owner = rest1.owner && "<ANONYMOUS>";
+// rest2.owner = rest2.owner && "<ANONYMOUS>";
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
+
+// ////----------------------The Nullish Coalescing Operator(??)-----------------
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// ///Nullish: null and undefined(NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 // console.log("-------------- OR -----------");
 // ////--------Use ANY data type, return ANY data type, short-circuiting----------
