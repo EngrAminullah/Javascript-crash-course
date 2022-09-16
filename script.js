66674b203eea3179d65785fd2d20b3a2f9dc16f7
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-09-16 12:12:18 +0500

    Coding challenge

--- script.js
+++ script.js
@@ -1087,7 +1087,7 @@ const game = {
   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
   date: "Nov 9th 2037",
   odds: {
-    team1: 1.33,
+    team1: 11.33,
     x: 3.25,
     team2: 6.5,
   },
@@ -1102,4 +1102,26 @@ const [gk, ...fieldPlayers] = players1;
 console.log(gk, fieldPlayers);
 
 /// 3.
-const allPlayers
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+/// 4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Periscic"];
+
+/// 5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+/// 6.
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+// printGoals(" Davies", "Muller", " LLewandowski", "Kimmich");
+// printGoals(" Davies", "Muller");
+printGoals(...game.scored);
+
+/// 7.
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
